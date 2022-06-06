73e1f25877c09cc19b63175cd7fe6a284661a245
Chirag M Deekshith <chiragmoorthy1@gmail.com>
2022-06-06 11:35:16 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# noq-service
+# NoQ - Service
+
+The backend server for the NoQ application. 
+Uses Spring Boot - Java 17.
---
+++ src/test/java/com/noqtech/noq/NoqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noqtech.noq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
