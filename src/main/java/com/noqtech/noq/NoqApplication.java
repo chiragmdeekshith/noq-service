b2c39e693ddc00933ba526e705492f66e712945d
Chirag M Deekshith <chiragmoorthy1@gmail.com>
2022-06-15 19:41:45 +0530

    Local sql db and CORS fix

--- src/main/java/com/noqtech/noq/NoqApplication.java
+++ src/main/java/com/noqtech/noq/NoqApplication.java
@@ -5,6 +5,9 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class NoqApplication {
@@ -19,5 +22,4 @@ public class NoqApplication {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         return modelMapper;
     }
-
 }
--- src/main/java/com/noqtech/noq/controller/RestaurantController.java
+++ src/main/java/com/noqtech/noq/controller/RestaurantController.java
@@ -9,15 +9,13 @@ import com.noqtech.noq.service.restaurant.RestaurantServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(name = "/")
 public class RestaurantController {
     @Autowired
